ed32300d27872e978d1a295865f86d3b8aa326f5
yassingaza24-source <yassingaza24@gmail.com>
2025-10-25 01:19:31 +0100

    Update autologon.bat

--- autologon.bat
+++ autologon.bat
@@ -1,17 +1,20 @@
-rem 1) Sauvegarde (copie avant tout)
 copy /Y "E:\Windows\System32\config\SOFTWARE" "E:\backup\SOFTWARE.bak"
+copy /Y "E:\Windows\System32\config\SYSTEM" "E:\backup\SYSTEM.bak"
 
-rem 2) Charger la ruche SOFTWARE dans HKLM\OFF_SOFTWARE
 reg load HKLM\OFF_SOFTWARE "E:\Windows\System32\config\SOFTWARE"
+reg load HKLM\OFF_SYSTEM "E:\Windows\System32\config\SYSTEM"
 
-rem 3) Créer/écrire les valeurs (remplace USER / PASS / PCNAME)
 reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_SZ /d 1 /f
 reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "<USER>" /f
 reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "<PASS>" /f
 reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultDomainName /t REG_SZ /d "<PCNAME>" /f
-
-rem (optionnel)
 reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v ForceAutoLogon /t REG_SZ /d 1 /f
 
-rem 4) Décharger la ruche
+reg add "HKLM\OFF_SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f
+
+reg add "HKLM\OFF_SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\DomainProfile" /v EnableFirewall /t REG_DWORD /d 0 /f
+reg add "HKLM\OFF_SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\PublicProfile" /v EnableFirewall /t REG_DWORD /d 0 /f
+reg add "HKLM\OFF_SYSTEM\CurrentControlSet\Services\SharedAccess\Parameters\FirewallPolicy\StandardProfile" /v EnableFirewall /t REG_DWORD /d 0 /f
+
 reg unload HKLM\OFF_SOFTWARE
+reg unload HKLM\OFF_SYSTEM
